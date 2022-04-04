51b71bbfcb998ba823e2003ed117e6e4546898e6
Mohammad Alka. R <99939707+alfkarmdhn@users.noreply.github.com>
2022-04-04 20:26:21 +0700

    Create Jamur.java

---
+++ Jamur.java
@@ -0,0 +1,14 @@
+
+package tugaspresentasi;
+
+public class Jamur {
+   void strukturTubuh() {
+        System.out.println("Bersifat multiseluler dan uniseluler");
+    }
+    void mencariMakanan() {
+        System.out.println("Bersifat eukariotik dan heterotrof");
+    }
+    void habitat() {
+        System.out.println("Hidup di air dan di darat");
+    }
+}
